311b3591295d6b3a55cd5e639094c5a05d726dda
Alexandr Bogomyakov <abogomyakov@iponweb.net>
2018-05-04 21:40:50 +0300

    Now this shit can complete any test with 100%

--- __app__.py
+++ __app__.py
@@ -1,11 +1,20 @@
 from main import Main
 
-
-username='4016014'
-password='40201'
+username = '4016014'
+password = '40201'
 
 app = Main(username, password)
 
 app.auth()
 app.get_tests()
-print(app.start_test(3,0))
+#print(app.start_test(3, 0))
+
+while True:
+    q = input('Subj >>> ')
+    a = input('Test >>> ')
+    app.resolve(int(q),int(a))
+    #q = input('Q >>> ')
+    #a = input('A >>> ')
+    #z = [a]
+    #app.answer(q, z)
+    
--- main.py
+++ main.py
@@ -50,7 +50,7 @@ class Main:
         for subject in soup.find_all(class_="dxeListBoxItem_Aqua dxeFTM"):
             if subject.get_text() != '\xa0':
                 self.subjects.update({subject.get_text(): []})
-                logging.info('Found subject %s', subject.get_text())
+                logging.debug('Found subject %s', subject.get_text())
         logging.info('Found %s subjects', len(self.subjects))
         # parse tests per subject
         i = 0
@@ -64,8 +64,8 @@ class Main:
             for test in soup.find_all(class_="dxeListBoxItem_Aqua dxeLTM"):
                 if test.get_text() != '\xa0':
                     self.subjects[subject].append(test.get_text())
-                    logging.info('%s - Found test %s' % (subject,
-                                                         test.get_text()))
+                    logging.debug('%s - Found test %s' % (subject,
+                                                          test.get_text()))
             i += 1
 
         #print(self.subjects)
@@ -74,11 +74,10 @@ class Main:
         # start test
         r = requests.post(
             settings.fesmu_root_url + 'studtst1.aspx',
-            data=settings.merge(
-                settings.scam_data_3, {
-                    'ctl00$MainContent$hfPred': pred,
-                    'ctl00$MainContent$hfTest': test
-                }),
+            data=settings.merge(settings.scam_data_3, {
+                'ctl00$MainContent$hfPred': pred,
+                'ctl00$MainContent$hfTest': test
+            }),
             cookies={'ASP.NET_SessionId': self.SessionId})
         # get test content
         r = requests.get(
@@ -86,7 +85,126 @@ class Main:
             cookies={'ASP.NET_SessionId': self.SessionId})
         soup = BeautifulSoup(r.text, "html.parser")
         _a = soup.find(class_='btntest')
-        q=[]
-        for i in _a.find_all(id=re.compile("ctl00_MainContent_ASPxButton.*_B")):
+        q = []
+        for i in _a.find_all(
+                id=re.compile("ctl00_MainContent_ASPxButton.*_B")):
             q.append(i)
-        return(len(q))
+        logging.info("Started subj %s, test %s" % (pred, test))
+        return (len(q))
+
+    # predict answers for any question
+    def predict(self, q_number):
+        # select question. just load its page.
+        answers = []
+
+        def parse(html):
+            a = []
+            soup = BeautifulSoup(html, "html.parser")
+            # find all correct answer
+            for i in soup.find_all(
+                    'span',
+                    attrs=
+                {
+                    'style':
+                    'display:inline-block;background-color:'\
+                    'Aquamarine;font-size:Medium;width:860px;'
+                }):
+                logging.debug(
+                    'Predicting answer for question #%s - %s | Answer number - %s'
+                    % (q_number, i.text, i.get('id')[-1:]))
+                a.append(int(i.get('id')[-1:]))
+            return a
+
+        requests.post(
+            settings.fesmu_root_url + 'studtst2.aspx',
+            data=settings.merge(
+                settings.scam_data_4,
+                {'ctl00$MainContent$ASPxButton' + str(q_number): ''}),
+            cookies={'ASP.NET_SessionId': self.SessionId})
+        # send all checkboxes as answer
+        requests.post(
+            settings.fesmu_root_url + 'studtst3.aspx',
+            data=settings.scam_data_5,
+            cookies={'ASP.NET_SessionId': self.SessionId})
+        # get answer page
+        r = requests.get(
+            settings.fesmu_root_url + 'studtst5.aspx',
+            cookies={'ASP.NET_SessionId': self.SessionId},
+            headers={'User-Agent': 'Mozilla/1337'})
+        answers = parse(r.text)
+        if len(answers) == 0:
+            logging.warning(
+                "There isn't any correct answers for %s. Looks like"\
+                " something went wrong. Trying other way...", q_number
+            )
+            # send all checkboxes as answer to 30th page
+            requests.post(
+                settings.fesmu_root_url + 'studtst2.aspx',
+                data=settings.merge(
+                    settings.scam_data_4,
+                    {'ctl00$MainContent$ASPxButton' + str(q_number): ''}),
+                cookies={'ASP.NET_SessionId': self.SessionId})
+
+            requests.post(
+                settings.fesmu_root_url + 'studtst30.aspx',
+                data=settings.scam_data_7,
+                cookies={'ASP.NET_SessionId': self.SessionId})
+            r = requests.get(
+                settings.fesmu_root_url + 'studtst5.aspx',
+                cookies={'ASP.NET_SessionId': self.SessionId},
+                headers={'User-Agent': 'Mozilla/1337'})
+            answers = parse(r.text)
+
+        logging.info("Prediction for question %s. Answers numbers - %s" %
+                     (q_number, answers))
+        return answers
+
+    def answer(self, q_number, answers):
+        def gen_a(a_count, answers):
+            a = {}
+            for i in range(1, a_count+1):
+                if i in answers:
+                    a['ctl00$MainContent$ASPxCheckBox' + str(i)] = 'C'
+                    a['ctl00$MainContent$hfo' + str(i)] = '1'
+                else:
+                    a['ctl00$MainContent$ASPxCheckBox' + str(i)] = 'U'
+                    a['ctl00$MainContent$hfo' + str(i)] = '0'
+            return a
+
+        if len(answers) == 0:
+            logging.warning("Incorrect answers count. Expected [1-5] got 0.")
+            return 1
+
+        # select question. just post to its page.
+        requests.post(
+            settings.fesmu_root_url + 'studtst2.aspx',
+            data=settings.merge(
+                settings.scam_data_4,
+                {'ctl00$MainContent$ASPxButton' + str(q_number): ''}),
+            cookies={'ASP.NET_SessionId': self.SessionId})
+        # mark and send correct answers
+        r = requests.post(
+            settings.fesmu_root_url + 'studtst3.aspx',
+            data=settings.merge(settings.scam_data_6, gen_a(5, answers)),
+            cookies={'ASP.NET_SessionId': self.SessionId})
+
+        if r.url == settings.fesmu_root_url + 'studtst30.aspx':
+            requests.post(
+                settings.fesmu_root_url + 'studtst2.aspx',
+                data=settings.merge(
+                    settings.scam_data_4,
+                    {'ctl00$MainContent$ASPxButton' + str(q_number): ''}),
+                cookies={'ASP.NET_SessionId': self.SessionId})
+            r = requests.post(
+                settings.fesmu_root_url + 'studtst30.aspx',
+                data=settings.merge(settings.scam_data_8, gen_a(4, answers)),
+                cookies={'ASP.NET_SessionId': self.SessionId})
+
+        logging.info("Send correct answer for %s. It's %s" % (q_number,
+                                                              answers))
+
+    def resolve(self, subj, test):
+        q_count = self.start_test(subj, test)
+        for i in range(1, q_count + 1):
+            prediction = self.predict(i)
+            self.answer(i, prediction)
