2de7d4bd569842e1639618ebade2a132ec4eed21
mk <xelnagamex@gmail.ru>
2018-05-11 20:46:37 +0200

    added baraban

---
+++ baraban.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Simple Bot to reply to Telegram messages.
+This is built on the API wrapper, see echobot2.py to see the same example built
+on the telegram.ext bot framework.
+This program is dedicated to the public domain under the CC0 license.
+"""
+import os
+import logging
+import telegram
+from telegram.error import NetworkError, Unauthorized
+from time import sleep
+
+update_id = None
+
+
+def main():
+    """Run the bot."""
+    global update_id
+    # Telegram Bot Authorization Token
+    TOKEN = os.environ["TG_TOKEN"]
+    bot = telegram.Bot(TOKEN)
+
+    # get the first pending update_id, this is so we can skip over it in case
+    # we get an "Unauthorized" exception.
+    try:
+        update_id = bot.get_updates()[0].update_id
+    except IndexError:
+        update_id = None
+
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    while True:
+        try:
+            echo(bot)
+        except NetworkError:
+            sleep(1)
+        except Unauthorized:
+            # The user has removed or blocked the bot.
+            update_id += 1
+
+def perdoliq(username, password, action, subj, test, acc):
+    from main import Perdoliq
+    app = Perdoliq(username, password)
+    app.auth()
+    app.get_tests()
+    app.resolve(subj, test, acc, is_delayed=False)
+    
+
+def echo(bot):
+    """Echo the message the user sent."""
+    global update_id
+    # Request updates after the last update_id
+    for update in bot.get_updates(offset=update_id, timeout=10):
+        update_id = update.update_id + 1
+        
+        if update.message:
+            s = update.message.text.split()
+            if len(s) == 6:
+                msg = "usr: " + s[0] + " pass: " + s[1] + " action: " + s[2] + " subj: " + s[3] + " test: " + s[4] + " acc: " + s[5]
+                perdoliq(s[0], s[1], s[2], s[3], s[4], s[5])
+                update.message.reply_text(msg)
+
+
+if __name__ == '__main__':
+    main()
